44e33c3747d7415ccd3ff77be89d53139fb512f5
R-level <47988410+R-level@users.noreply.github.com>
2023-12-15 21:05:30 +0200

    Create Find_Intersection.java

---
+++ Problems/CoderByte/src/Find_Intersection.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class FInd_Intersection {
+
+        public static String FindIntersection(String[] strArr) {
+            // a list for containing the arrays of Strings
+            ArrayList<List<String>> list = new ArrayList<>();
+            //Smallest size array
+            int min=Integer.MAX_VALUE;
+            //For every string in the strArr
+            for(String s:strArr){
+
+                String[] numberList = s.split(", ");
+                //Find min which is  the smallest length of all the arrays.
+                if(numberList.length<min){
+                    min = numberList.length;
+                }
+                list.add(Arrays.asList(numberList));
+            }
+
+            //Create a new result list
+            //List<String> result = new ArrayList<String>();
+            StringBuilder result = new StringBuilder("");
+            //Find matches between the lists
+            for(int i=0;i<min;i++){
+                if(list.get(1).contains(list.get(0).get(i))){
+                    //result.add(list.get(0).get(i));
+                    result.append(list.get(0).get(i));
+
+                    if(i!=min-1){
+                        result.append(",");
+                    }
+                }
+            }
+            //Take the results together and create a string
+            return result.toString();
+        }
+
+        public static void main (String[] args) {
+
+            System.out.print(FindIntersection(new String[] {"1, 3, 4, 7, 13", "1, 2, 4, 13, 15"}));
+        }
+}
