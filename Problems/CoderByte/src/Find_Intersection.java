b2323423817a9b7d56a8fead03a3be62c0bfe529
R-level <47988410+R-level@users.noreply.github.com>
2023-12-16 14:27:29 +0200

    Additional improvements
    Extracted a method and pasted a new method solution found on Coderbyte.

--- Problems/CoderByte/src/Find_Intersection.java
+++ Problems/CoderByte/src/Find_Intersection.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+import static java.util.stream.Collectors.toCollection;
+
 public class Find_Intersection {
 
         public static String FindIntersection(String[] strArr) {
@@ -22,22 +24,38 @@ public class Find_Intersection {
             //List<String> result = new ArrayList<String>();
             StringBuilder result = new StringBuilder("");
             //Find matches between the lists
-            for(int i=0;i<min;i++){
-                if(list.get(1).contains(list.get(0).get(i))){
-                    //result.add(list.get(0).get(i));
-                    result.append(list.get(0).get(i));
-
-                    if(i!=min-1){
-                        result.append(",");
-                    }
-                }
-            }
+            result = findMatches(list, min, result);
             //Take the results together and create a string
             return result.toString();
         }
 
+    private static StringBuilder findMatches(ArrayList<List<String>> list, int min, StringBuilder result) {
+        for(int i = 0; i< min; i++){
+            if(list.get(1).contains(list.get(0).get(i))){
+                //result.add(list.get(0).get(i));
+                result.append(list.get(0).get(i));
+
+                if(i!= min -1){
+                    result.append(",");
+                }
+            }
+        }
+        return result;
+    }
+
+    //Answer found at https://coderbyte.com/solution/Find%20Intersection
+    public static String FindIntersectionv2(String[] strArr) {
+        TreeSet<Integer> set1 = Arrays.stream(strArr[0].replace(" ", "").split(",")).map(Integer::parseInt).collect(toCollection(TreeSet::new));
+        TreeSet<Integer> set2 = Arrays.stream(strArr[1].replace(" ", "").split(",")).map(Integer::parseInt).collect(toCollection(TreeSet::new));
+        set1.retainAll(set2);
+
+        return (set1.isEmpty() ? "false" : Arrays.toString(new TreeSet<>(set1).toArray()).replace("]", "").replace("[", "").replace(" ", ""));
+    }
+
         public static void main (String[] args) {
 
-            System.out.print(FindIntersection(new String[] {"1, 3, 4, 7, 13", "1, 2, 4, 13, 15"}));
+            System.out.println(FindIntersection(new String[] {"1, 3, 4, 7, 13", "1, 2, 4, 13, 15"}));
+            System.out.println(FindIntersectionv2(new String[] {"1, 3, 4, 7, 13", "1, 2, 4, 13, 15"}));
+
         }
 }
