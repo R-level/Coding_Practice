7f505ebc6612fe909b00b2d1354f614272fba2ab
R-level <47988410+R-level@users.noreply.github.com>
2023-12-19 13:06:16 +0200

    Added improved code
    Added improved versions of Add_Two_Number algorithm. One recursive and one method with pointers

--- Problems/LeetCode/src/Add_2_Numbers/Add_2_numbers.java
+++ Problems/LeetCode/src/Add_2_Numbers/Add_2_numbers.java
@@ -25,8 +25,47 @@ public class Add_2_numbers {
          ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3,null)));
          ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4, null)));
        System.out.println(addTwoNumbers(l1,l2).toString());
+       System.out.println(improvedAddTwoNumbers(l1,l2).toString());
+        System.out.println(recursiveAddTwoNumbers(l1,l2).toString());
     }
-        public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    private static ListNode improvedAddTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int digit1 = (l1 != null) ? l1.val : 0;
+            int digit2 = (l2 != null) ? l2.val : 0;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            ListNode newNode = new ListNode(digit);
+            tail.next = newNode;
+            tail = tail.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+        ListNode result = dummyHead.next;
+        dummyHead.next = null;
+        return result;
+     }
+    public static ListNode recursiveAddTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        else if (l1 == null) return l2;
+        else if (l2 == null) return l1;
+
+        int a = l1.val + l2.val;
+        ListNode p = new ListNode(a % 10);
+        p.next = recursiveAddTwoNumbers(l1.next, l2.next);
+        if (a >= 10) p.next = recursiveAddTwoNumbers(p.next, new ListNode(1));
+        return p;
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
             StringBuilder number1 = getListNumbers(l1);
             StringBuilder number2 = getListNumbers(l2);
