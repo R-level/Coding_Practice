eaac0bdce3590e32ec172dd5609b7470c2b17628
R-level <47988410+R-level@users.noreply.github.com>
2023-12-19 09:30:36 +0200

    Uploaded solution and update
    Upload of new solution Add_2_numbers and updated ReadMe in Largest Palindrome

---
+++ Problems/LeetCode/src/Add_2_Numbers/Add_2_numbers.java
@@ -0,0 +1,71 @@
+package Add_2_Numbers;
+
+public class Add_2_numbers {
+
+    // Definition for singly-linked list.
+     public static class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     @Override
+        public String toString(){
+              ListNode head = this;
+              StringBuilder s = new StringBuilder();
+              do {
+             s.append(head.val);
+             head = head.next;
+         }while(head!=null);
+              return s.toString();
+     }
+     }
+
+    public static void main(String[] args) {
+         ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3,null)));
+         ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4, null)));
+       System.out.println(addTwoNumbers(l1,l2).toString());
+    }
+        public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+            StringBuilder number1 = getListNumbers(l1);
+            StringBuilder number2 = getListNumbers(l2);
+
+            int n1= Integer.parseInt(number1.toString());
+            int n2= Integer.parseInt(number2.toString());
+
+            int sum = n1 + n2;
+
+            return createList(sum);
+        }
+
+        public static StringBuilder getListNumbers(ListNode list){
+            StringBuilder s = new StringBuilder();
+
+            do{ s.append(list.val);
+                list = list.next;
+            }while(list!=null);
+
+            return s.reverse();
+        }
+
+        public static ListNode createList(int sum){
+            String sumString =  String.valueOf(sum);
+
+            int i =0;
+            ListNode head = null;
+            while(i<=sumString.length()-1){
+                if(i==0)
+                { head = new ListNode(Integer.parseInt(String.valueOf(sumString.charAt(i))),null);
+                }
+                else{
+                    ListNode node1 = new ListNode(Integer.parseInt(String.valueOf(sumString.charAt(i))),head);
+                    head = node1;
+                }
+                i++;
+            }
+            return head;
+        }
+    }
+
+
